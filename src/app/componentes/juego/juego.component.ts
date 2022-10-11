c8f8fe39781245596b030278e346cc7bf6917bd5
Diego Rojas <Diego-27ro@outlook.com>
2022-10-11 09:10:44 -0500

    [182727486] add service theme and keyboard component

--- src/app/componentes/juego/juego.component.ts
+++ src/app/componentes/juego/juego.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ThemeService} from './service/theme.service';
 @Component({
   selector: 'app-juego',
   templateUrl: './juego.component.html',
   styleUrls: ['./juego.component.css']
 })
 export class JuegoComponent implements OnInit {
+  bacgroundImgUrl = '';
 
-  constructor() { }
+  constructor(
+    private themeService: ThemeService
+  ) { }
 
   ngOnInit(): void {
+    this.themeService.getThemeImages()
+    .subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
--- src/app/componentes/juego/juego.module.ts
+++ src/app/componentes/juego/juego.module.ts
@@ -4,17 +4,22 @@ import { CommonModule } from '@angular/common';
 import { JuegoRoutingModule } from './juego-routing.module';
 import { JuegoComponent } from './juego.component';
 import {SharedModule} from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+//components
 import { GenericButtonComponent } from './generic-button/generic-button.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
 
 @NgModule({
   declarations: [
     JuegoComponent,
     GenericButtonComponent,
+    KeyboardComponent,
   ],
   imports: [
     CommonModule,
     JuegoRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ]
 })
 export class JuegoModule { }
