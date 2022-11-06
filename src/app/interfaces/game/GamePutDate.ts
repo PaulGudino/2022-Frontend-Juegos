b78d394be2ca934d21e5af7e14b75efebb404784
Diego Rojas <Diego-27ro@outlook.com>
2022-11-06 18:50:59 -0500

    [#182727534] finish change date availability game

---
+++ src/app/interfaces/game/GamePutDate.ts
@@ -0,0 +1,9 @@
+export interface  GamePutDate{
+   id : string,
+   start_date: string;
+   end_date: string;
+   modification_date : string;
+   game : string;
+   is_active : string;
+
+ }
---
+++ src/app/servicios/game/game.service.ts
@@ -0,0 +1,25 @@
+import { GamePutDate } from 'src/app/interfaces/game/gamePutDate';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PuenteDatosService } from '../comunicacio_componentes/puente-datos.service';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  url = this.puente.geturl();
+
+
+  constructor(
+   private http: HttpClient,
+   private puente: PuenteDatosService
+  ) { }
+
+
+  putGame(id: number, data: any){
+   return this.http.put(this.url+'api/game/'+id+'/', data);
+ }
+}
