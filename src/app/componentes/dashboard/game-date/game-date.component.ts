74d92457fa74ca1a018b74f15c356a020d328323
Paul Gudiño <pgudino@espol.edu.ec>
2022-11-28 12:33:13 -0500

    Game Data

--- src/app/componentes/dashboard/game-date/game-date.component.ts
+++ src/app/componentes/dashboard/game-date/game-date.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SnackbarService } from 'src/app/servicios/snackbar/snackbar.service';
 import {GameService} from './../../../servicios/game/game.service'
 import { GamePutDate } from 'src/app/interfaces/game/GamePutDate';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game-date',
@@ -10,16 +11,46 @@ import { GamePutDate } from 'src/app/interfaces/game/GamePutDate';
 })
 export class GameDateComponent implements OnInit {
 
-   beginDate: Date = new Date(2015,10,30);
-   finishDate: Date = new Date();
+   minDate = new Date();
+   minDatefin  = new Date();
 
+   startDate: Date = new Date();
+   endDate: Date = new Date();
+
+   form: FormGroup;
+   beginDate: Date = new Date();
+   finishDate: Date = new Date();
    constructor(
       private snackbar:SnackbarService,
       private game:GameService,
-   ) { }
+      private fb: FormBuilder,
+   ) {
+      this.form = this.fb.group({
+         startTime:['', Validators.required],
+         endTime:['', Validators.required],
+       })
+    }
 
    ngOnInit(): void {
    }
+
+   async changetime(){
+      let hora_inicio = this.form.value.startTime.hour
+      let minuto_inicio = this.form.value.startTime.minute
+  
+      let hora_fin = this.form.value.endTime.hour
+      let minuto_fin = this.form.value.endTime.minute
+    
+  
+      let inicio_date = this.beginDate.toISOString().split('T')[0];
+      const [year_i, month_i, day_i] = inicio_date.split('-');
+  
+      let fin_date = this.finishDate.toISOString().split('T')[0];
+      const [year_f, month_f, day_f] = fin_date.split('-');
+  
+      this.startDate = new Date(parseInt(year_i), parseInt(month_i) - 1, parseInt(day_i), parseInt(hora_inicio), parseInt(minuto_inicio));
+      this.endDate = new Date(parseInt(year_f), parseInt(month_f) - 1, parseInt(day_f), parseInt(hora_fin), parseInt(minuto_fin));
+    }
 
    saveNewDateGame(){
       if(this.validateDates()){
