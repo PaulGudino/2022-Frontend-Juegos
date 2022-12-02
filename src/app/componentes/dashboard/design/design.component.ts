bdd6755e1341259df2eb84be248aa6ef51053437
Diego Rojas <Diego-27ro@outlook.com>
2022-12-02 17:30:57 -0500

    solve errors

--- src/app/componentes/dashboard/design/design.component.ts
+++ src/app/componentes/dashboard/design/design.component.ts
@@ -1,177 +1,201 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
-import {DashboardPublicityService} from '../../../servicios/publicity/dashboardPublicity/dashboard-publicity.service'
-import {SnackbarService} from 'src/app/servicios/snackbar/snackbar.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DashboardPublicityService } from '../../../servicios/publicity/dashboardPublicity/dashboard-publicity.service';
+import { SnackbarService } from 'src/app/servicios/snackbar/snackbar.service';
 import { PublicityService } from 'src/app/servicios/publicity/publicity.service';
 import { ThemeService } from 'src/app/servicios/theme/theme.service';
 import { ImageService } from 'src/app/servicios/image/image.service';
 import { ConfirmDialogService } from 'src/app/servicios/confirm-dialog/confirm-dialog.service';
 import { DashboardStyleService } from 'src/app/servicios/theme/dashboardStyle/dashboard-style.service';
+import { Form } from '@angular/forms';
 @Component({
-  selector: 'app-design',
-  templateUrl: './design.component.html',
-  styleUrls: ['./design.component.css']
+   selector: 'app-design',
+   templateUrl: './design.component.html',
+   styleUrls: ['./design.component.css'],
 })
 export class DesignComponent implements OnInit {
-   availableSpin:string= 'Disponible 1 Giro más!';
-   fontFamily:string=''
-   colorText:string=''
-
+   availableSpin: string = 'Disponible 1 Giro más!';
+   fontFamily: string = '';
+   colorText: string = '';
 
    previsulizacionMachine: string = '';
    previsulizacionLogo: string = '';
-   @ViewChild("takeInput", { static: false })
+   @ViewChild('takeInput', { static: false })
    InputVar!: ElementRef;
 
-   @ViewChild("takeInputLogo", { static: false })
+   @ViewChild('takeInputLogo', { static: false })
    InputVarLogo!: ElementRef;
 
    fileToUploadMachine!: File | null;
-   imagenMachine!:File;
+   imagenMachine!: File;
 
    fileToUploadLogo!: File | null;
-   imagenLogo!:File;
+   imagenLogo!: File;
 
-  constructor(
-   private dashboardPublicityService: DashboardPublicityService,
-   private publicity: PublicityService,
-   // private router: Router,
-   private snackbar: SnackbarService,
-   private dialogService: ConfirmDialogService,
-   private theme: ThemeService,
-   public dashStyle: DashboardStyleService,
-   private imageSrv: ImageService,
-  ) { }
+   constructor(
+      private dashboardPublicityService: DashboardPublicityService,
+      private publicity: PublicityService,
+      // private router: Router,
+      private snackbar: SnackbarService,
+      private dialogService: ConfirmDialogService,
+      private theme: ThemeService,
+      public dashStyle: DashboardStyleService,
+      private imageSrv: ImageService
+   ) {}
 
-  ngOnInit(): void {
-   this.publicity.getPublicityList().subscribe(
-      (data => {
-         console.log(data[0])
+   ngOnInit(): void {
+      this.publicity.getPublicityList().subscribe((data) => {
+         console.log(data[0]);
          this.dashboardPublicityService.loadData(data);
-         this.dashboardPublicityService.changeTopPublicityImage(data[0].image)
-         this.dashboardPublicityService.changeBottomPublicityImage(data[1].image)
-         this.theme.getDesignInformation().subscribe(
-            (designData) => {
-               this.dashStyle.loadData(designData[0]);
-               this.previsulizacionMachine = this.dashStyle.get_image_machine_game();
-               this.previsulizacionLogo = this.dashStyle.get_image_logo_game();
-               this.fontFamily = this.dashStyle.get_font_letter();
-               this.colorText = this.dashStyle.get_color_text();
-               console.log(designData[0])
-            }
-         )
-
-      })
-   )
-  }
-
-  capturarFile(event: any): void {
-
-   this.fileToUploadMachine = this.imageSrv.captureFile(event);
-
-   if (this.fileToUploadMachine) {
-      this.imagenMachine = this.fileToUploadMachine;
-      this.imageSrv.extraerBase64(this.fileToUploadMachine).then((imagenMachine: any) => {
-      this.previsulizacionMachine = imagenMachine.base;
-      this.dashStyle.setImageMchineGameFile(this.fileToUploadMachine)
-
-     });
+         this.dashboardPublicityService.changeTopPublicityImage(data[0].image);
+         this.dashboardPublicityService.changeBottomPublicityImage(
+            data[1].image
+         );
+         this.theme.getDesignInformation().subscribe((designData) => {
+            this.dashStyle.loadData(designData[0]);
+            this.previsulizacionMachine =
+               this.dashStyle.get_image_machine_game();
+            this.previsulizacionLogo = this.dashStyle.get_image_logo_game();
+            this.fontFamily = this.dashStyle.get_font_letter();
+            this.colorText = this.dashStyle.get_color_text();
+            console.log(designData[0]);
+         });
+      });
    }
-   else{
-     this.InputVar.nativeElement.value = "";
-     this.InputVarLogo.nativeElement.value = "";
-     this.snackbar.mensaje('Solo se permiten imagenes');
-   }
-  }
-  capturarFileLogo(event:any):void{
-   this.fileToUploadLogo = this.imageSrv.captureFile(event);
-   if(this.fileToUploadLogo){
-      this.imagenLogo = this.fileToUploadLogo;
-      this.imageSrv.extraerBase64(this.fileToUploadLogo).then((imagenLogo: any) => {
-         this.previsulizacionLogo = imagenLogo.base;
-         this.dashStyle.setImageLogoFile(this.fileToUploadLogo)
 
-        });
+   capturarFile(event: any): void {
+      this.fileToUploadMachine = this.imageSrv.captureFile(event);
 
-   }else{
-      this.InputVarLogo.nativeElement.value = "";
-      this.snackbar.mensaje('Solo se permiten imagenes');
-    }
-
-
-  }
-
-  updateDesign(){
-   const options = {
-      title: 'ACTUALIZAR Salvapantallas',
-      message: '¿ESTÁ SEGURO QUE DESEA ACTUALIZAR EL SALVAPANTALLAS?',
-      cancelText: 'CANCELAR',
-      confirmText: 'CREAR'
-    };
-   this.dialogService.open(options);
-   this.dialogService.confirmed().subscribe(confirmed => {
-      if(confirmed && this.fileToUploadMachine){
-         let formData: FormData = new FormData();
-         formData.append('id','1')
-         formData.append('image_machine_game', this.dashStyle.getImageMachineGameFile(), this.dashStyle.getImageMachineGameFile().name);
-         formData.append('color_text',this.colorText)
-         formData.append('font_letter',this.fontFamily)
-         formData.append('date_modified',new Date().toISOString())
-         formData.append('is_active','true')
-         formData.append('game_id','1')
-
-         this.theme.updateDesgin(1,formData);
-
-         //this.router.navigate(['/dashboard/juego/fecha']);
-         this.snackbar.mensaje("Salvapantallas Actualizado exitosamente");
-
-      }else if(confirmed && this.fileToUploadLogo){
-         let formData: FormData = new FormData();
-         formData.append('id','1')
-         formData.append('image_logo_game', this.dashStyle.getImageLogoFile(), this.dashStyle.getImageLogoFile().name);
-         formData.append('color_text',this.colorText)
-         formData.append('font_letter',this.fontFamily)
-         formData.append('date_modified',new Date().toISOString())
-         formData.append('is_active','true')
-         formData.append('game_id','1')
-
-         this.theme.updateDesgin(1,formData);
-
-         //this.router.navigate(['/dashboard/juego/fecha']);
-         this.snackbar.mensaje("Salvapantallas Actualizado exitosamente");
-
-
-      }else if(confirmed && !this.fileToUploadMachine && !this.fileToUploadLogo){
-         let formData: FormData = new FormData();
-         formData.append('id','1')
-         formData.append('color_text',this.colorText)
-         formData.append('font_letter',this.fontFamily)
-         formData.append('date_modified',new Date().toISOString())
-         formData.append('is_active','true')
-         formData.append('game_id','1')
-         this.theme.updateDesgin(1,formData);
-
-         //this.router.navigate(['/dashboard/juego/fecha']);
-         this.snackbar.mensaje("Salvapantallas Actualizado exitosamente");
-
+      if (this.fileToUploadMachine) {
+         this.imagenMachine = this.fileToUploadMachine;
+         this.imageSrv
+            .extraerBase64(this.fileToUploadMachine)
+            .then((imagenMachine: any) => {
+               this.previsulizacionMachine = imagenMachine.base;
+               this.dashStyle.setImageMchineGameFile(this.fileToUploadMachine);
+            });
+      } else {
+         this.InputVar.nativeElement.value = '';
+         this.InputVarLogo.nativeElement.value = '';
+         this.snackbar.mensaje('Solo se permiten imagenes');
       }
+   }
 
-   })
+   capturarFileLogo(event: any): void {
+      this.fileToUploadLogo = this.imageSrv.captureFile(event);
+      if (this.fileToUploadLogo) {
+         this.imagenLogo = this.fileToUploadLogo;
+         this.imageSrv
+            .extraerBase64(this.fileToUploadLogo)
+            .then((imagenLogo: any) => {
+               this.previsulizacionLogo = imagenLogo.base;
+               this.dashStyle.setImageLogoFile(this.fileToUploadLogo);
+            });
+      } else {
+         this.InputVarLogo.nativeElement.value = '';
+         this.snackbar.mensaje('Solo se permiten imagenes');
+      }
+   }
 
-  }
+   updateDesign() {
+      const options = {
+         title: 'ACTUALIZAR Salvapantallas',
+         message: '¿ESTÁ SEGURO QUE DESEA ACTUALIZAR EL SALVAPANTALLAS?',
+         cancelText: 'CANCELAR',
+         confirmText: 'CREAR',
+      };
+      this.dialogService.open(options);
+      this.dialogService.confirmed().subscribe((confirmed) => {
+         if (confirmed) {
+            if (this.fileToUploadMachine && this.fileToUploadLogo) {
+               console.log('entra a la doble condicion');
+               let formMachine: FormData = this.updateMachineImage();
+               formMachine.append(
+                  'image_logo_game',
+                  this.dashStyle.getImageLogoFile(),
+                  this.dashStyle.getImageLogoFile().name
+               );
+               this.theme.updateDesgin(1, formMachine);
+               this.snackbar.mensaje('Salvapantallas Actualizado exitosamente');
+            } else if (this.fileToUploadMachine) {
+               console.log('ACTUALIZACION SOLO CON LA IMAGEN MACHINE');
+               let form = this.updateMachineImage();
+               this.theme.updateDesgin(1, form);
+               this.snackbar.mensaje('Salvapantallas Actualizado exitosamente');
+               //this.router.navigate(['/dashboard/juego/fecha']);
+            } else if (this.fileToUploadLogo) {
+               console.log('ACTUALIZACION SOLO CON LA IMAGEN LOGO');
+               this.updateLogoImage();
+               console.log(
+                  'SE EJECUTO LA ACTUALIZACION SOLO CON LA IMAGEN LOGO'
+               );
+            } else if (!this.fileToUploadMachine && !this.fileToUploadLogo) {
+               let formData: FormData = new FormData();
+               formData.append('id', '1');
+               formData.append('color_text', this.colorText);
+               formData.append('font_letter', this.fontFamily);
+               formData.append('date_modified', new Date().toISOString());
+               formData.append('is_active', 'true');
+               formData.append('game_id', '1');
+               this.theme.updateDesgin(1, formData);
 
-  previewChangeFontLetter(event:Event){
-   let element:HTMLSelectElement = event.target as HTMLSelectElement;
-   this.fontFamily = element.value;
-  }
-  previewChangeColorLetter(event:Event){
-   let element:HTMLSelectElement = event.target as HTMLSelectElement;
-   this.colorText = element.value;
-   console.log(this.colorText);
-  }
-  cancel(){
-   this.fontFamily = this.dashStyle.get_font_letter();
-   this.colorText = this.dashStyle.get_color_text();
+               //this.router.navigate(['/dashboard/juego/fecha']);
+               this.snackbar.mensaje('Salvapantallas Actualizado exitosamente');
+            }
+         }
+      });
+   }
 
-  }
+   updateMachineImage(): FormData {
+      let formData: FormData = new FormData();
+      formData.append('id', '1');
+      formData.append(
+         'image_machine_game',
+         this.dashStyle.getImageMachineGameFile(),
+         this.dashStyle.getImageMachineGameFile().name
+      );
+      formData.append('color_text', this.colorText);
+      formData.append('font_letter', this.fontFamily);
+      formData.append('date_modified', new Date().toISOString());
+      formData.append('is_active', 'true');
+      formData.append('game_id', '1');
+      return formData;
 
+      // this.theme.updateDesgin(1, formData);
+      // this.snackbar.mensaje('Salvapantallas Actualizado exitosamente');
+      console.log(` en machine ${formData}`);
+   }
+   private updateLogoImage() {
+      console.log('updateLogoImage');
+      let formData: FormData = new FormData();
+      formData.append('id', '1');
+      formData.append(
+         'image_logo_game',
+         this.dashStyle.getImageLogoFile(),
+         this.dashStyle.getImageLogoFile().name
+      );
+      formData.append('color_text', this.colorText);
+      formData.append('font_letter', this.fontFamily);
+      formData.append('date_modified', new Date().toISOString());
+      formData.append('is_active', 'true');
+      formData.append('game_id', '1');
+      console.log(` en LOGO ${formData}`);
+
+      this.theme.updateDesgin(1, formData);
+      //this.router.navigate(['/dashboard/juego/fecha']);
+      this.snackbar.mensaje('Salvapantallas Actualizado exitosamente');
+   }
+
+   previewChangeFontLetter(event: Event) {
+      let element: HTMLSelectElement = event.target as HTMLSelectElement;
+      this.fontFamily = element.value;
+   }
+   previewChangeColorLetter(event: Event) {
+      let element: HTMLSelectElement = event.target as HTMLSelectElement;
+      this.colorText = element.value;
+      console.log(this.colorText);
+   }
+   cancel() {
+      this.fontFamily = this.dashStyle.get_font_letter();
+      this.colorText = this.dashStyle.get_color_text();
+   }
 }
--- src/app/componentes/dashboard/publicity/publicity.component.ts
+++ src/app/componentes/dashboard/publicity/publicity.component.ts
@@ -1,119 +1,119 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicityService } from 'src/app/servicios/publicity/publicity.service';
-import {DashboardPublicityService} from '../../../servicios/publicity/dashboardPublicity/dashboard-publicity.service'
-import {SnackbarService} from 'src/app/servicios/snackbar/snackbar.service';
+import { DashboardPublicityService } from '../../../servicios/publicity/dashboardPublicity/dashboard-publicity.service';
+import { SnackbarService } from 'src/app/servicios/snackbar/snackbar.service';
 import { ConfirmDialogService } from 'src/app/servicios/confirm-dialog/confirm-dialog.service';
 import { Router } from '@angular/router';
 import { ContentObserver } from '@angular/cdk/observers';
 
 @Component({
-  selector: 'app-publicity',
-  templateUrl: './publicity.component.html',
-  styleUrls: ['./publicity.component.css']
+   selector: 'app-publicity',
+   templateUrl: './publicity.component.html',
+   styleUrls: ['./publicity.component.css'],
 })
 export class PublicityComponent implements OnInit {
+   constructor(
+      private publicity: PublicityService,
+      private dashboardPublicityService: DashboardPublicityService,
+      // private router: Router,
+      private snackbar: SnackbarService,
+      private dialogService: ConfirmDialogService
+   ) {}
 
+   ngOnInit(): void {
+      this.publicity.getPublicityList().subscribe((data) => {
+         console.log(data[0]);
+         this.dashboardPublicityService.loadData(data);
+         this.dashboardPublicityService.changeTopPublicityImage(data[0].image);
+         this.dashboardPublicityService.changeBottomPublicityImage(
+            data[1].image
+         );
+      });
+   }
 
+   createPublicity() {}
 
-  constructor(
-   private publicity: PublicityService,
-   private dashboardPublicityService: DashboardPublicityService,
-   // private router: Router,
-   private snackbar: SnackbarService,
-   private dialogService: ConfirmDialogService,
-
-  ) { }
-
-  ngOnInit(): void {
-   this.publicity.getPublicityList().subscribe(
-      (data => {
-            console.log(data[0])
-            this.dashboardPublicityService.loadData(data);
-            this.dashboardPublicityService.changeTopPublicityImage(data[0].image)
-            this.dashboardPublicityService.changeBottomPublicityImage(data[1].image)
-      })
-   )
-  }
-
-	createPublicity(){
-
-	}
-
-	updatePublicity(){
+   updatePublicity() {
       const options = {
          title: 'ACTUALIZAR PUBLICIDAD',
          message: '¿ESTÁ SEGURO QUE DESEA ACTUALIZAR LA PUBLICIDAD?',
          cancelText: 'CANCELAR',
-         confirmText: 'CREAR'
-       };
+         confirmText: 'CREAR',
+      };
 
       this.dialogService.open(options);
-      this.dialogService.confirmed().subscribe(confirmed => {
-         if(confirmed){
-            if(this.dashboardPublicityService.getChangeTop()){
+      this.dialogService.confirmed().subscribe((confirmed) => {
+         if (confirmed) {
+            if (
+               this.dashboardPublicityService.getChangeTop() &&
+               this.dashboardPublicityService.getChangeBottom()
+            ) {
                this.updateTop();
-               this.snackbar.mensaje("Publicidad Actualizada exitosamente");
-               console.log('se agrega top')
-
-
-            }else if(this.dashboardPublicityService.getChangeBottom()){
                this.updateBottom();
-               this.snackbar.mensaje("Publicidad Actualizada exitosamente");
-
-
-            }else if(this.dashboardPublicityService.getChangeBottom() && this.dashboardPublicityService.getChangeTop()){
+               this.snackbar.mensaje('Publicidad Actualizada exitosamente');
+               console.log('se agrega top y bottom');
+            } else if (this.dashboardPublicityService.getChangeBottom()) {
                this.updateBottom();
+               this.snackbar.mensaje('Publicidad Actualizada exitosamente');
+            } else if (this.dashboardPublicityService.getChangeTop()) {
                this.updateTop();
-               this.snackbar.mensaje("Publicidad Actualizada exitosamente");
-
-            }else if(this.dashboardPublicityService.getTopPublicity().image == null){
-               this.snackbar.mensaje("No existe publicidad para agregar o actualizar");
-
-
+               this.snackbar.mensaje('Publicidad Actualizada exitosamente');
+            } else if (
+               this.dashboardPublicityService.getTopPublicity().image == null &&
+               this.dashboardPublicityService.getBottomPublicity().image == null
+            ) {
+               this.snackbar.mensaje(
+                  'No existe publicidad para agregar o actualizar'
+               );
             }
-            console.log('al guardar')
-            console.log(this.dashboardPublicityService.getTopPublicity())
-
 
             // this.publicity.put(1,formDataTop);
             // this.publicity.put(2,formDataBottom);
             // //this.router.navigate(['/dashboard/juego/fecha']);
-
          }
-
-      })
-
-
+      });
    }
 
-   private updateTop(){
+   private updateTop() {
       let formDataTop: FormData = new FormData();
-      formDataTop.append('id','1')
-      formDataTop.append('image', this.dashboardPublicityService.getTopImageFile(), this.dashboardPublicityService.getTopImageFile().name);
-      formDataTop.append('titulo','Publicidad Superior')
-      formDataTop.append('created',this.dashboardPublicityService.getTopPublicity().created)
-      formDataTop.append('modified',new Date().toISOString())
-      formDataTop.append('is_active','true')
-      this.publicity.put(1,formDataTop);
+      formDataTop.append('id', '1');
+      formDataTop.append(
+         'image',
+         this.dashboardPublicityService.getTopImageFile(),
+         this.dashboardPublicityService.getTopImageFile().name
+      );
+      formDataTop.append('titulo', 'Publicidad Superior');
+      formDataTop.append(
+         'created',
+         this.dashboardPublicityService.getTopPublicity().created
+      );
+      formDataTop.append('modified', new Date().toISOString());
+      formDataTop.append('is_active', 'true');
+      this.publicity.put(1, formDataTop);
       this.dashboardPublicityService.setChangeTop(false);
-
    }
-   private updateBottom(){
+   private updateBottom() {
       let formDataBottom: FormData = new FormData();
-      formDataBottom.append('id','2')
-      formDataBottom.append('image', this.dashboardPublicityService.getBottomImageFile(), this.dashboardPublicityService.getBottomImageFile().name);
-      formDataBottom.append('titulo','Publicidad Superior')
-      formDataBottom.append('created',this.dashboardPublicityService.getBottomPublicity().created)
-      formDataBottom.append('modified',new Date().toISOString())
-      formDataBottom.append('is_active','true')
-      this.publicity.put(2,formDataBottom);
-      this.dashboardPublicityService.setChangeBottom(false)
-
+      formDataBottom.append('id', '2');
+      formDataBottom.append(
+         'image',
+         this.dashboardPublicityService.getBottomImageFile(),
+         this.dashboardPublicityService.getBottomImageFile().name
+      );
+      formDataBottom.append('titulo', 'Publicidad Inferior');
+      formDataBottom.append(
+         'created',
+         this.dashboardPublicityService.getBottomPublicity().created
+      );
+      formDataBottom.append('modified', new Date().toISOString());
+      formDataBottom.append('is_active', 'true');
+      this.publicity.put(2, formDataBottom);
+      this.dashboardPublicityService.setChangeBottom(false);
    }
-   signalChangeTop(){
+   signalChangeTop() {
       this.dashboardPublicityService.setChangeTop(true);
    }
-   signalChangeBottom(){
+   signalChangeBottom() {
       this.dashboardPublicityService.setChangeBottom(true);
    }
 }
