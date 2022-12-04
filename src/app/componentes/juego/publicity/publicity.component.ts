3a99629fc20e4846eda1ba35658a395c67b36cf3
Diego Rojas <Diego-27ro@outlook.com>
2022-12-04 00:30:30 -0500

    solve error in publicity animation

--- src/app/componentes/juego/publicity/publicity.component.ts
+++ src/app/componentes/juego/publicity/publicity.component.ts
@@ -19,7 +19,7 @@ export class PublicityComponent implements OnInit {
          let width = this.animationCount * -100;
          this.scrollContainer.nativeElement.style.transform = `translateX(${width}%)`;
          this.scrollContainer.nativeElement.style.transition =
-            'transform .3s easey 1s';
+            'transform .3s cubic-bezier(0.61, 1, 0.88, 1)';
 
          if (this.animationCount == this.publicityList.length) {
             setTimeout(() => {
@@ -31,9 +31,10 @@ export class PublicityComponent implements OnInit {
                this.animationCount = 0;
             });
          } else {
-            this.animationCount++;
+            if (this.animationCount < this.publicityList.length)
+               this.animationCount++;
          }
-      }, 3000);
+      }, 4000);
    }
 
    ngOnInit(): void {
