43893b250de8beb0a68c8fce5590d7bcc6b20e40
Diego David <diego-27ro@outlook.com>
2022-12-26 18:09:00 -0500

    solve errors

--- src/app/componentes/juego/publicity/publicity.component.ts
+++ src/app/componentes/juego/publicity/publicity.component.ts
@@ -1,7 +1,6 @@
+import { Publicity } from './../../../interfaces/publicity/publicity';
 import { PublicityService } from 'src/app/servicios/publicity/publicity.service';
 import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
-import { DashboardPublicityService } from 'src/app/servicios/publicity/dashboardPublicity/dashboard-publicity.service';
-import { Publicity } from '../../../interfaces/publicity/publicity';
 
 @Component({
    selector: 'app-publicity',
@@ -13,11 +12,29 @@ export class PublicityComponent implements OnInit {
    scrollContainer!: ElementRef;
    @Input() publicityList: Publicity[] = [];
    animationCount: number = 0;
-   @Input() timeInterval: number = 1000;
+   @Input() isTop: boolean = true;
+   timeIntervalTop: number = 1000;
+   timeIntervalBottom: number = 1000;
 
-   constructor() {
-      setInterval(() => {
-         console.log('interval ' + this.animationCount);
+   constructor(private publicity: PublicityService) {}
+
+   ngOnInit(): void {
+      this.publicity.getPublicityConfigTop().subscribe((data) => {
+         this.timeIntervalTop = data.time_display * 1000;
+         this.publicity.getPublicityConfigBottom().subscribe((data) => {
+            this.timeIntervalBottom = data.time_display * 1000;
+            this.isTop
+               ? this.createInterval(this.timeIntervalTop)
+               : this.createInterval(this.timeIntervalBottom);
+         });
+      });
+   }
+
+   createInterval(timeInterval: number) {
+      let intervalId: any;
+      intervalId = setInterval(() => {
+         console.log('interval ' + timeInterval);
+         console.log('intervalId ' + intervalId);
          let width = this.animationCount * -100;
          this.scrollContainer.nativeElement.style.transform = `translateX(${width}%)`;
          this.scrollContainer.nativeElement.style.transition =
@@ -36,8 +53,6 @@ export class PublicityComponent implements OnInit {
             if (this.animationCount < this.publicityList.length)
                this.animationCount++;
          }
-      }, this.timeInterval);
+      }, timeInterval);
    }
-
-   ngOnInit(): void {}
 }
